23caf989ba5a1eb74599cd65a85d45a12d3c927d
Shannon Wood <shannon@4woods.us>
2019-02-20 16:48:02 -0700

    Completed footer (fixed bluma issues).

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <?php $template_uri = get_template_directory_uri(); ?>
 
 <footer>
-  <div class="footer">
-    <div class="footerText">
+  <div class="foot">
+    <div class="footText">
       <p>Ould Fellas is a 501(c)(3) nonprofit organization, operates the nation's largest
 sanctuary for homeless, senior dogs.</p>
     </div>
 
-    <div class="footerIcons">
-      <ul class="footerIconsUL">
+    <div class="footIcons">
+      <ul class="footIconsUL">
         <li><a href="https://www.facebook.com"><img src="<?php echo $template_uri . '/media/imgs/Facebook.png';?>"></a></li>
         <li><a href="https://www.twitter.com"><img src="<?php echo $template_uri . '/media/imgs/Twitter.png';?>"</a></li>
         <li><a href="https://www.instagram.com"><img src="<?php echo $template_uri . '/media/imgs/Instagram.png';?>"></a></li>
--- header.php
+++ header.php
@@ -5,7 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
   <?php wp_head(); ?>
-  <title>Document</title>
+  <title>Ye Ould Geezer</title>
 </head>
 <body>
