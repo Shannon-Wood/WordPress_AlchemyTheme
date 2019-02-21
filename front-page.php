e39b361ef6877c8e1a97aefb0150f554d5cd8a38
Alicia Wilson <alicia@thearrayschool.com>
2019-02-21 15:39:33 -0700

    finished blog page

---
+++ front-page.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+
+<div class="hero">
+  <p class="hero-text"><em>You’ll never get the dog you want,<br> you’ll get the dog you need.<br> ― Cesar Millan</em></p>
+</div>
+
+<div class="card-feature">
+  <div class="columns">
+    <div class="column is-one-third">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/residents-block.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column is-one-third">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/sponsorship-blocks.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column is-one-third">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/in-memoriam-block.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="dog-bone-button">
+    <a href="#"><div class="bone-img"></div></a>
+  </div>
+</div>
+
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
-
-<div class="hero">
-  <p class="hero-text"><em>You’ll never get the dog you want,<br> you’ll get the dog you need.<br> ― Cesar Millan</em></p>
-</div>
-
-<div class="card-feature">
+<div class="blog-container">
+  <div class="paws">
+    <div class="bone-img" id="blog-bone"></div>
+  </div>
+  <div class="header-card">
+    <div class="blog-main-img"></div>
+    <div class="blog-main-text">Lorem ipsum dolor sit amet, ad sed populo lobortis periculis, commodo forensibus et quo. In graeci aliquid vis. Vel eu suas adipiscing posidonium, dicam fastidii forensibus at qui, ad duo vero copiosae reprimique. An ipsum theophrastus quo, has ut stet malis etiam. Et sit ridens omnium, labore argumentum vis cu.</div>
+  </div>
   <div class="columns">
-    <div class="column is-one-third">
+    <div class="column">
       <div class="card">
         <div class="card-image">
           <figure class="image is-4by3">
-            <img src="<?php echo get_template_directory_uri() . '/img/residents-block.jpg';?>" alt="Placeholder image">
+            <img src="<?php echo get_template_directory_uri() . '/img/story-one.jpg';?>" alt="Placeholder image">
           </figure>
         </div>
         <div class="card-content">
@@ -20,11 +22,11 @@
         </div>
       </div>
     </div>
-    <div class="column is-one-third">
+    <div class="column">
       <div class="card">
         <div class="card-image">
           <figure class="image is-4by3">
-            <img src="<?php echo get_template_directory_uri() . '/img/sponsorship-blocks.jpg';?>" alt="Placeholder image">
+            <img src="<?php echo get_template_directory_uri() . '/img/story-two.jpg';?>" alt="Placeholder image">
           </figure>
         </div>
         <div class="card-content">
@@ -34,11 +36,11 @@
         </div>
       </div>
     </div>
-    <div class="column is-one-third">
+    <div class="column">
       <div class="card">
         <div class="card-image">
           <figure class="image is-4by3">
-            <img src="<?php echo get_template_directory_uri() . '/img/in-memoriam-block.jpg';?>" alt="Placeholder image">
+            <img src="<?php echo get_template_directory_uri() . '/img/story-three.jpg';?>" alt="Placeholder image">
           </figure>
         </div>
         <div class="card-content">
@@ -49,7 +51,50 @@
       </div>
     </div>
   </div>
-  <div class="dog-bone-button">
-    <a href="#"><div class="bone-img"></div></a>
+  <div class="columns">
+    <div class="column">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/health-block.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/play-block.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/news-block.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
+<div class="paws"></div>
+<?php get_footer(); ?>
