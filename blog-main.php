6f6292efac4253dc8ce95da80cdba4645362a7a4
Alicia Wilson <alicia@thearrayschool.com>
2019-02-21 16:49:01 -0700

    finished single post page

---
+++ blog-main.php
@@ -0,0 +1,100 @@
+<?php get_header(); ?>
+<div class="page-container">
+  <div class="paws">
+    <div class="bone-img" id="blog-bone"></div>
+  </div>
+  <div class="header-card">
+    <div class="blog-main-img"></div>
+    <div class="blog-main-text">Lorem ipsum dolor sit amet, ad sed populo lobortis periculis, commodo forensibus et quo. In graeci aliquid vis. Vel eu suas adipiscing posidonium, dicam fastidii forensibus at qui, ad duo vero copiosae reprimique. An ipsum theophrastus quo, has ut stet malis etiam. Et sit ridens omnium, labore argumentum vis cu.</div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      <div class="card" id="single-post-card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/story-one.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/story-two.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/story-three.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/health-block.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/play-block.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="<?php echo get_template_directory_uri() . '/img/news-block.jpg';?>" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, at corrumpit mediocritatem nec, nonumes aliquando concludaturque id sit. Soleat albucius nec ex, no sonet vocent offendit sed. Ea meis debet scaevola vis.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="paws"></div>
+<?php get_footer(); ?>
